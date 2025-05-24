59c0f60c5f2af245f097a0945efbbbba33082eee
Priiyyya-21 <priyamajhi79@gmail.com>
2025-05-24 20:21:20 +0530

    Added the start and stop assistant function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,99 @@ import {vapi,startAssistant,stopAssistant} from "./ai"
 
 
 function App() {
-  
+   const [started, setStarted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [assistantIsSpeaking, setAssistantIsSpeaking] = useState(false);
+  const [volumeLevel, setVolumeLevel] = useState(0);
+  const [callId, setCallId] = useState("");
+  const [callResult, setCallResult] = useState(null);
+  const [loadingResult, setLoadingResult] = useState(false);
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(()=>{
+    vapi.on("call-start",()=>{
+      setLoading(false)
+      setStarted(true)
+    }).on("call-end",()=>{
+      setStarted(false)
+      setLoading(false)
+    }).on("speech-start",()=>{
+      setAssistantIsSpeaking(true)
+    }).on("speech-end",()=>{
+      setAssistantIsSpeaking(false)
+    }).on("volume-level",(level)=>{
+      setVolumeLevel(level)
+    })
+  },[])
+
+  
+  const handleInputChange=(setter)=>(event)=>{
+    setter(event.target.value)
+  }
+
+  const handleStart=async()=>{
+    setLoading(true)
+    const data=await startAssistant(firstName,lastName,email,phoneNumber)
+    setCallId(data.id)
+  }
+
+  const handleStop=()=>{
+    stopAssistant()
+    //get call details
+  }
+
+  const showForm = !loading && !started && !loadingResult && !callResult
+  const allFieldsFilled= firstName && lastName && email && phoneNumber
   return (
-    <>
-    </>
+    <div className='app-container'>
+      {
+        showForm && 
+        <>
+        <h1>Contact Details (Required)</h1>
+        <input
+        type="text"
+        placeholder='First Name'
+        value={firstName}
+        className='input-field'
+        onChange={handleInputChange(setFirstName)}
+        />
+        <input
+        type="text"
+        placeholder='Last Name'
+        value={lastName}
+        className='input-field'
+        onChange={handleInputChange(setLastName)}
+        />
+        <input
+        type="email"
+        placeholder='Email address'
+        value={email}
+        className='input-field'
+        onChange={handleInputChange(setEmail)}
+        />
+        <input
+        type="tel"
+        placeholder='Phone number'
+        value={phoneNumber}
+        className='input-field'
+        onChange={handleInputChange(setPhoneNumber)}
+        />
+
+        {!started &&(
+          <button onClick={handleStart}
+          disabled={!allFieldsFilled}
+          className='button'>
+            Start Application Call
+          </button>
+        ) }
+        </>
+      }
+    {(loading || loadingResult) && <div className='loading'></div>}
+    </div>
   )
 }
 
