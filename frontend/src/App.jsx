4fa5db5d74308f8778da95e1a13c5e0cb9d78a92
Priiyyya-21 <priyamajhi79@gmail.com>
2025-05-24 19:30:58 +0530

    Create AI voice assistant project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import {vapi,startAssistant,stopAssistant} from "./ai"
+
+
+function App() {
+  
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
